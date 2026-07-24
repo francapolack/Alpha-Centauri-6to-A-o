05321adb8cd69e603712b9c789223aaeca358ff2
francapolack <frmuuoo@gmail.com>
2026-07-24 08:42:44 -0300

    Debugging, mostrar texto anda mal :(

--- Alpha_Centauri/Mercurio.py
+++ Alpha_Centauri/Mercurio.py
@@ -32,77 +32,68 @@ fondo_rect=fondo.get_rect()
 #DEFINICION DE CLASES,FUNCIONES Y OBJETOS 
 ANCHO_PC,ALTO_PC=pygame.display.get_surface().get_size()
 def texto_display(texto,fuente,color):
-    txt=fuente.render(texto,True,color,)
-    pantalla.blit(txt,(x,y))
+    x=ANCHO_PC//2
+    y=ALTO_PC-int(ALTO_PC*0.14)
 
     ancho_caja=int(ANCHO_PC*0.90)
     alto_caja=int(ALTO_PC*0.22)
     caja_rect=pygame.Rect(0,0,ancho_caja,alto_caja)
-    x=ANCHO_PC//2
-    y=ALTO_PC-int(ALTO_PC*0.14)
+
     caja_rect.center = (x,y)
     pygame.draw.rect(pantalla, (20, 20, 35), caja_rect, border_radius=8)
     pygame.draw.rect(pantalla, (0, 200, 220), caja_rect, width=3, border_radius=8)
-    
+
+    txt=fuente.render(texto,True,color,)
+    pantalla.blit(txt,(x,y))
+
 
 class Jugador:
-    def __init__(self,textura):
-        self.textura=textura
-        self.hitbox=self.textura.get_rect()
-        self.hitbox.center=(ANCHO_PC//2,ALTO_PC//2)
-    def movimiento(self):
-        tecla=pygame.key.get_pressed()
-        if tecla[pygame.K_LEFT] or tecla[pygame.K_a]:
-            self.hitbox.move_ip(-JUGADOR_VELOCIDAD,0)
-
-        elif tecla[pygame.K_RIGHT] or tecla[pygame.K_d]:
-            self.hitbox.move_ip(JUGADOR_VELOCIDAD,0)
-
-        elif tecla[pygame.K_UP] or tecla[pygame.K_w]:
-            self.hitbox.move_ip(0,-JUGADOR_VELOCIDAD)
-
-        elif tecla[pygame.K_DOWN] or tecla[pygame.K_s]:
-            self.hitbox.move_ip(0,JUGADOR_VELOCIDAD)
-
-class Partes_Rover:
-    #poner esto como una libreria?
-    click=False 
     def __init__(self,textura):
         self.textura_inicial=textura
         self.textura=pygame.transform.scale(self.textura_inicial,(150,50))
         self.hitbox=self.textura.get_rect()
-        self.hitbox.center=(ANCHO_PC//4,ALTO_PC//8)
+        self.hitbox.center=(ANCHO_PC//2,ALTO_PC//2)
+
+
+class Partes_Rover:
+    #poner esto como una libreria?
+    click=False 
+    def __init__(self,textura,x):
+        self.textura_inicial=textura
+        self.textura=pygame.transform.scale(self.textura_inicial,(150,50))
+        self.hitbox=self.textura.get_rect()
+        self.hitbox.center=(x,(ALTO_PC//4))
     def descripcion(self,texto):
         self.texto=texto
-
+""""
 class Boton:
     def __init__(self,texto,fuente,color_texto):
-        self.hitbox=pygame.Rect(0,0,180,40)
+        self.boton=pygame.Rect(0,0,180,40)
         self.color_fondo=(0,230,150)
-        pygame.draw.rect(pantalla,self.color_fondo,self.hitbox,border_radius=5)
+        pygame.draw.rect(pantalla,self.color_fondo,self.boton,border_radius=5)
         
         self.fuente_boton=fuente
         self.txt=self.fuente_boton.render(texto,True,color_texto)
-        self.txt_rect=self.txt.get_rect(center=self.hitbox.center)
-        pantalla.blit(self.txt,self.txt_rect)
+        self.hitbox=self.txt.get_rect(center=self.boton.center)
+        pantalla.blit(self.txt,self.hitbox)
 
 termine=Boton
 termine("Termine de construir",FUENTE_DIALOGOS,BLANCO)
-    
+"""
 
 textura_jugador=pygame.image.load("Alpha-Centauri-6to-A-o/Alpha_Centauri/imagenes/camina_adelante.png").convert_alpha()
 jugador=Jugador(textura_jugador)
 
 textura_rueda=pygame.image.load("Alpha-Centauri-6to-A-o/Alpha_Centauri/imagenes/objetos/mercurio/ROVER_RUEDA.png").convert_alpha()
-rueda=Partes_Rover(textura_rueda)
+rueda=Partes_Rover(textura_rueda,(ANCHO_PC//2))
 rueda.descripcion("Texto de la rueda, bla bla blaaa")
 
 textura_camara=pygame.image.load("Alpha-Centauri-6to-A-o/Alpha_Centauri/imagenes/objetos/mercurio/ROVER_CAMARAS.png").convert_alpha()
-camara=Partes_Rover(textura_camara)
+camara=Partes_Rover(textura_camara,(ANCHO_PC//4))
 camara.descripcion("Texto de la camata, bla bla blaa")
 
 textura_base=pygame.image.load("Alpha-Centauri-6to-A-o/Alpha_Centauri/imagenes/objetos/mercurio/ROVER_BASE.png").convert_alpha()
-base=Partes_Rover(textura_base)
+base=Partes_Rover(textura_base,(ANCHO_PC//6))
 base.descripcion("Texto de la base,bla bla blaa")
 
 class Main:
@@ -117,20 +108,35 @@ class Main:
             #SELECCION DE LAS PARTEEESSSS
             elif event.type==pygame.MOUSEBUTTONDOWN:
                 if base.hitbox.collidepoint(event.pos):
-                    texto_display(base.texto,NEGRO)
+                    texto_display(base.texto,FUENTE_DIALOGOS,NEGRO)
                     SELECCIONADOS.append("BASE")
                 elif rueda.hitbox.collidepoint(event.pos):
-                    texto_display(rueda.texto,NEGRO)
+                    texto_display(rueda.texto,FUENTE_DIALOGOS,NEGRO)
                     SELECCIONADOS.append("RUEDA")
                 elif camara.hitbox.collidepoint(event.pos):
-                    texto_display(camara.texto,NEGRO)
+                    print("Es el texto display")
+                    texto_display(camara.texto,FUENTE_DIALOGOS,NEGRO)
                     SELECCIONADOS.append("CÁMARA")
-                elif termine.hitbox.collidepoint(event.pos):
-                    if len(SELECCIONADOS)>=3:
-                        texto_display("Has terminado de construir tu rover!")
-                elif len(SELECCIONADOS)<3:
-                    texto_display("Renee: Seguro de que has terminado de construir?")
+                #elif termine.hitbox.collidepoint(event.pos):
+                    #if len(SELECCIONADOS)>=3:
+                        #texto_display("Has terminado de construir tu rover!",FUENTE_DIALOGOS,NEGRO)
+               # elif len(SELECCIONADOS)<3:
+                    #texto_display("Renee: Seguro de que has terminado de construir?",FUENTE_DIALOGOS,NEGRO)
         fisica.step(1/60.0)
+
+        #MOVIMIENTO DEL JUGRADOR
+        tecla=pygame.key.get_pressed()
+        if tecla[pygame.K_LEFT] or tecla[pygame.K_a]:
+            jugador.hitbox.move_ip(-JUGADOR_VELOCIDAD,0)
+
+        elif tecla[pygame.K_RIGHT] or tecla[pygame.K_d]:
+            jugador.hitbox.move_ip(JUGADOR_VELOCIDAD,0)
+
+        elif tecla[pygame.K_UP] or tecla[pygame.K_w]:
+            jugador.hitbox.move_ip(0,-JUGADOR_VELOCIDAD)
+
+        elif tecla[pygame.K_DOWN] or tecla[pygame.K_s]:
+            jugador.hitbox.move_ip(0,JUGADOR_VELOCIDAD)
 
         pantalla.fill(BLANCO)
         pantalla.blit(fondo,fondo_rect)
